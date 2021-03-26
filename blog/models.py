fe72f012e9809babc4ff5e73a5136048e14c4a07
janhorak58 <janhorak58@gmail.com>
2021-03-26 13:03:18 +0100

    complete website

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    pub_date = models.DateTimeField(auto_now_add=True)
+    pub_date = models.DateField()
     body = models.CharField(max_length=1000)
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:120] + "...."
+
+    def __str__(self):
+        return self.title
